2961a135d6686cf3554ff26c0d330f4771e1037b
Guilherme H. l. dos Santos <125158701+GuilhermeLimaSantos@users.noreply.github.com>
2023-11-16 17:06:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ const loginDiv = document.querySelector("login")
 const dashboardDiv = document.querySelector("dashboard")
 
 function {
-  
+  loginDiv.display.style = "nome"
+  dashboardDiv.display = "block"
 }
